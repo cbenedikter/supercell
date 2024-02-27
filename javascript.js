c380efdbf4e7e2fa829894f13d1666d83eac1971
cbenedikter <84837890+cbenedikter@users.noreply.github.com>
2024-02-27 17:48:51 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,60 +1,61 @@
-
-const transparencyBtn = document.getElementById('transparencyBtn');
-const goldPassLink = document.getElementById('goldPassLink');
-const extraImageLink = document.getElementById('extraImageLink');
-const popup = document.getElementById('popup');
-
-let transparencyClicked = false;
-
-// Toggle transparency of the button
-transparencyBtn.addEventListener('click', () => {
-    transparencyClicked = !transparencyClicked;
-    transparencyBtn.style.opacity = transparencyClicked ? '0.5' : '1';
-});
-
-// Toggle border of the gold pass image when clicked
-goldPassLink.addEventListener('click', (e) => {
-        e.stopPropagation(); // Prevent click event from bubbling up to document
-    goldPassLink.classList.toggle('clicked');
-    console.log("Gold Pass clicked");
-       popup.style.display = 'block'; 
+document.addEventListener('DOMContentLoaded', () => {
+    const transparencyBtn = document.getElementById('transparencyBtn');
+    const goldPassLink = document.getElementById('goldPassLink');
+    const extraImageLink = document.getElementById('extraImageLink');
+    const popup = document.getElementById('popup');
+    
+    let transparencyClicked = false;
+    
+    // Toggle transparency of the button
+    transparencyBtn.addEventListener('click', () => {
+        transparencyClicked = !transparencyClicked;
+        transparencyBtn.style.opacity = transparencyClicked ? '0.5' : '1';
+    });
+    
+    // Toggle border of the gold pass image when clicked
+    goldPassLink.addEventListener('click', (e) => {
+            e.stopPropagation(); // Prevent click event from bubbling up to document
+        goldPassLink.classList.toggle('clicked');
+        console.log("Gold Pass clicked");
+           popup.style.display = 'block'; 
    
-    const tags = { 
-cart_update: Math.floor(Date.now() / 1000),
-product_name: "Gold Pass",
- KEY_03: "VALUE_03"
-};
-OneSignal.User.addTags(tags);
-
-         console.log("Tags Sent: 'cart_update' : " + tags.cart_update + " 'product_name' : " + tags.product_name + " 'KEY_03' : " + tags.KEY_03 );
-
-});
-
-
-// Toggle border of the extra image when clicked
-extraImageLink.addEventListener('click', (e) => {
-       e.stopPropagation();
-    extraImageLink.classList.toggle('clicked');
-    console.log("Chest of Gems clicked");
-    popup.style.display = 'block'; 
- 
-    const tags = { 
-cart_update: Math.floor(Date.now() / 1000),
- product_name: "Chest of Gems",
- KEY_03: "VALUE_03"
-};
-OneSignal.User.addTags(tags);
-      console.log("Tags Sent: 'cart_update' : " + tags.cart_update + " 'product_name' : " + tags.product_name + " 'KEY_03' : " + tags.KEY_03 );
-
-});
-
-
-
-// Close popup when clicked outside of it
-document.addEventListener('click', (e) => {
-    if (e.target !== popup) {
-        popup.style.display = 'none'; // Hide the popup
-    }
+        const tags = { 
+    cart_update: Math.floor(Date.now() / 1000),
+    product_name: "Gold Pass",
+     KEY_03: "VALUE_03"
+    };
+    OneSignal.User.addTags(tags);
+    
+             console.log("Tags Sent: 'cart_update' : " + tags.cart_update + " 'product_name' : " + tags.product_name + " 'KEY_03' : " + tags.KEY_03 );
+    
+    });
+    
+    
+    // Toggle border of the extra image when clicked
+    extraImageLink.addEventListener('click', (e) => {
+           e.stopPropagation();
+        extraImageLink.classList.toggle('clicked');
+        console.log("Chest of Gems clicked");
+        popup.style.display = 'block'; 
+     
+        const tags = { 
+    cart_update: Math.floor(Date.now() / 1000),
+     product_name: "Chest of Gems",
+     KEY_03: "VALUE_03"
+    };
+    OneSignal.User.addTags(tags);
+          console.log("Tags Sent: 'cart_update' : " + tags.cart_update + " 'product_name' : " + tags.product_name + " 'KEY_03' : " + tags.KEY_03 );
+    
+    });
+    
+    
+    
+    // Close popup when clicked outside of it
+    document.addEventListener('click', (e) => {
+        if (e.target !== popup) {
+            popup.style.display = 'none'; // Hide the popup
+        }
+    });
 });
 
 // Add event listeners to handle clicks on Gold Pass Image and Extra Image buttons
