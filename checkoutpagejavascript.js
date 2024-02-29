d549467a9670efdd81ffb21684245f52b1251497
cbenedikter <84837890+cbenedikter@users.noreply.github.com>
2024-02-29 09:54:32 +0000

    Update checkoutpagejavascript.js

--- checkoutpagejavascript.js
+++ checkoutpagejavascript.js
@@ -1,1 +1,22 @@
+// Show console log and remove data tags when Clear Cart button is clicked
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM Content Loaded");
+    const clearCartButton = document.getElementById('clearCart');
+    clearCartButton.addEventListener('click', () => {
+        const tags =  ['cart_update' , 'product_name'];
+        OneSignal.User.removeTags(tags);
+        console.log(tags);
+    });
+});
+
+
+// Send a custom outcome to OneSignal when Check out button is clicked
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM Content Loaded");
+    const checkoutButton = document.getElementById('checkout');
+    checkoutButton.addEventListener('click', () => {
+        OneSignal.Session.sendOutcome("Purchase", 1);
+        console.log("Purchase Outcome sent to OneSignal");
+    });
+});
 
